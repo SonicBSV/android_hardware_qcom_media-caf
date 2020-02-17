88bf79312a6d4da1c0e10be657674239e5b608ab
SonicBSV <sonicbsv@gmail.com>
2020-02-17 23:20:05 +0300

    Compatibility for X00T

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
-
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -9,19 +7,6 @@ include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 include $(QCOM_MEDIA_ROOT)/libaac/Android.mk
 endif
 
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
-include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
-
-ifeq ($(BOARD_USES_ADRENO), true)
-include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
-endif
-
-include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
-
 ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
-endif
-
-endif
-
 endif
